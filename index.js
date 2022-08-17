b5949259483f52708dcb25bc747911b6a4e9b3fc
ElikanahMasanta <emasanta8@gmail.com>
2022-08-17 10:57:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation){
+    const scuberHeadquaters = 42;
+    return Math.abs(pickUpLocation - scuberHeadquaters)
+  }
+
+  function distanceFromHqInFeet(pickUpLocation){
+    distanceFromHqInBlocks(pickUpLocation);
+    const scuberHeadquaters = 264;
+    return Math.abs(pickUpLocation - scuberHeadquaters)
+  }
+
+  function distanceTravelledInFeet(startBlock, EndBlock) {
+      
+  }
+    distanceTravelledInFeet(43,48)
